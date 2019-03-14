99bfed0a43bae4f15c295569832801bfed03e1c1
Hemant <hemant.goswami@digital.homeoffice.gov.uk>
2019-03-14 14:35:33 +0000

    Removes duplicate lines

--- app.js
+++ app.js
@@ -65,8 +65,6 @@ app.use(flash())
 app.use(lusca.csrf())
 app.use(lusca.xframe('SAMEORIGIN'))
 app.use(lusca.xssProtection(true))
-app.use(lusca.xframe('SAMEORIGIN'))
-app.use(lusca.xssProtection(true))
 app.use((req, res, next) => {
   res.locals.user = req.user
   next()
