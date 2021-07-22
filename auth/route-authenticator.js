95956c7ca460bd2fb64fb5c9bafe0068cc9f0f9e
Josh Rowe <s-block@users.noreply.github.com>
2021-07-22 17:13:02 +0100

    Remove logging

--- auth/route-authenticator.js
+++ auth/route-authenticator.js
@@ -4,9 +4,6 @@ const authorise = require('./access-control').authorise
 const routeAuthenticator = (resource, permission, attribute = null) => {
   return (req, res, next) => {
     logger.log('debug', 'Running route authenticator')
-    logger.log('debug', 'process.env.GAIT_ROLE: ' + process.env.GAIT_ROLE)
-    logger.log('debug', 'process.env.GAIT_SUPERUSER_ROLE: ' + process.env.GAIT_SUPERUSER_ROLE)
-    logger.log('debug', 'process.env.GAIT_READONLY_ROLE: ' + process.env.GAIT_READONLY_ROLE)
     req.app.locals = req.app.locals || {}
     const userRoles = req.app.locals.roles || []
     logger.log('debug', 'Route authenticate for routes ' + userRoles.toString())
