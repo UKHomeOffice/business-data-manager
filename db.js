c6e804e557adf8065684901e4c1b4436b08d25c7
Hemant Goswami <hemantgoswami@users.noreply.github.com>
2025-02-06 15:36:55 +0000

    Implements SSL based RDS connection (#114)
    * Updates the connection string to use ssl
    
    * Updates the ssl config
    
    * Updating node-pg package
    
    * Updates db config
    
    * Implements certificate for RDS connection

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const pg = require('pg')
+const fs = require('fs')
+const production = process.env.NODE_ENV === 'production'
 
 const config = require('./config/core')
 
@@ -10,6 +12,11 @@ const pool = new pg.Pool({
   database: config.pg.database,
   password: config.pg.password,
   port: config.pg.port,
+  ssl: {
+    rejectUnauthorized: production,
+    ca: fs.readFileSync('./config/certs/eu-west-2-bundle.pem')
+      .toString()
+  }
 })
 
 module.exports = pool
